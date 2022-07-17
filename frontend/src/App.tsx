03111d9b262a63fee49cfbc6fd3bb632b47d36f0
helber <helber@infox.com.br>
2022-07-17 03:54:18 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./componentes/Header";
 import NotificationButton from "./componentes/NotificationButton";
 import SalesCard from "./componentes/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
